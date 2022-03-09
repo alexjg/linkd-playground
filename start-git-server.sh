93483caeefbe1dc87256a6885e39f7ab3a45d1e7
Alex Good <alex@memoryandthought.me>
2022-03-09 10:38:51 +0000

    stuff

--- start-git-server.sh
+++ start-git-server.sh
@@ -1,6 +1,24 @@
 export $(cat env)
 
-cargo run \
+cargo build \
     --target-dir ./target \
     --manifest-path $LINK_CHECKOUT/bins/Cargo.toml \
-    -p linkd-git -- /tmp/socket-activated-home 127.0.0.1:9876
+    --release \
+    -p linkd-git;
+peer_id=$(LNK_HOME=/tmp/socket-activated-home \
+    cargo run \
+        --target-dir ./target \
+        --manifest-path $LINK_CHECKOUT/bins/Cargo.toml \
+        --release \
+        -p lnk -- profile peer);
+
+systemd-socket-activate \
+    -l 9987 \
+    --fdname=ssh \
+    -E SSH_AUTH_SOCK \
+    -E RUST_BACKTRACE \
+    -E RUST_LOG \
+    ./target/release/linkd-git \
+    /tmp/socket-activated-home \
+    --linkd-rpc-socket $XDG_RUNTIME_DIR/link-peer-$peer_id-rpc.socket \
+    --linger-timeout 10000
--- start-seed.sh
+++ start-seed.sh
@@ -6,6 +6,6 @@ cargo build \
     --release \
     -p linkd;
 ./target/release/linkd \
-    --rad-home /tmp/seed-home \
+    --lnk-home /tmp/seed-home \
     --track everything \
     --protocol-listen 127.0.0.1:8799
